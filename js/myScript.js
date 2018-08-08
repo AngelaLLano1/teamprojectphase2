e1742be4ed170afeab4919ac81c4d0502790da8c
Angela Llano <angelallano96@gmail.com>
2018-08-08 16:46:50 +0000

    final  commit

--- js/myScript.js
+++ js/myScript.js
@@ -10,4 +10,20 @@ $(document).ready(function() {
     });
     
 
+window.onload = function () {
+   document.getElementById('button').onclick = function() {
+     if(document.getElementById("code").value == '10')
+        location.href = "https://preview.c9users.io/angelall/group-project-phase2/pages/youth.html";
+     else if(document.getElementById("code").value == '20')
+     location.href = "https://preview.c9users.io/angelall/group-project-phase2/pages/adult.html";
+     else  if(document.getElementById("code").value == '30')
+     location.href = "https://preview.c9users.io/angelall/group-project-phase2/pages/senior.html";
+     else
+        //Create a div or p to show message
+        document.getElementById("message").innerHTML = 'Please Select Your Age Group First';
+     return false;//prevent form submission
+   }//.onclick
+}//window.onload
+//End Of Code.
+
   
